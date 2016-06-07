339594a8ec899a97e5603cc0ef8814ab33a4fafd
spencer <spencer.dossett@gmail.com>
2016-06-07 17:23:52 -0400

    made translate pass all the tests

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,3 +1,16 @@
 def translate(text)
-
+  vowels = ['a', 'e', 'i', 'o', 'y']
+  result = ""
+  text.split(' ').each do |word|
+    word.each_char do |chr|
+      if vowels.include? chr
+        middle = word.split(chr, 2)[0]
+        beginning = chr + word.split(chr, 2)[1]
+        result += " " if result.length > 0
+        result += "#{beginning}#{middle}ay"
+        break
+      end
+    end
+  end
+  result
 end
