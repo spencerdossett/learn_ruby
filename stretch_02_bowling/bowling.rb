05bb4bb0290fc8eb8d1bab79256246e1417d7e82
spencer <spencer.dossett@gmail.com>
2016-06-08 17:47:49 -0400

    improved the score method in Game class - 3 fails left

--- stretch_02_bowling/bowling.rb
+++ stretch_02_bowling/bowling.rb
@@ -1,9 +1,28 @@
 class Game
   def initialize
-    @frame = 1
+    @frame = 0
+    @score = 0
   end
 
-  def score(frames)
+  def score(balls_rolled)
+    strike_bonus = false
+    strike_bonus2 = false
+
+    balls_rolled.each do |pins_hit|
+      @score += pins_hit
+      # @score += pins_hit if spare_bonus = true
+      @score += pins_hit if strike_bonus2
+      @score += pins_hit if strike_bonus
+
+      strike_bonus2 = strike_bonus
+      strike_bonus = false
+      strike_bonus = true if pins_hit == 10
+      # spare_bonus = false
+      # spare_bonus = true if
+
+    end
+
+    @score
 
   end
 end
