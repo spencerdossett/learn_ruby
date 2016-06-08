cc41d6efea8f4248ad84dd1eb6caf3e1d6b2aa51
spencer <spencer.dossett@gmail.com>
2016-06-08 17:17:14 -0400

    first pass at self.make_change for Changer class

--- stretch_01_make_change/changer.rb
+++ stretch_01_make_change/changer.rb
@@ -1,3 +1,12 @@
 class Changer
-
+  def self.make_change(amount_in_cents)
+    change = []
+    coins = [25, 10, 5, 1]
+    coins.each do |coin|
+      if amount_in_cents >= coin
+        change << coin
+        amount_in_cents -= coin
+      end
+    end
+  end
 end
