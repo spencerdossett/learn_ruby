9e3999420d791387ab24b1013186b68aa60d0292
spencer <spencer.dossett@gmail.com>
2016-06-08 17:25:42 -0400

    second pass at self.make_change. It works!

--- stretch_01_make_change/changer.rb
+++ stretch_01_make_change/changer.rb
@@ -2,11 +2,17 @@ class Changer
   def self.make_change(amount_in_cents)
     change = []
     coins = [25, 10, 5, 1]
-    coins.each do |coin|
-      if amount_in_cents >= coin
-        change << coin
-        amount_in_cents -= coin
+    while amount_in_cents > 0 do
+      coins.each do |coin|
+        if amount_in_cents >= coin
+          change << coin
+          amount_in_cents -= coin
+          break
+          # puts amount_in_cents
+        end
       end
     end
+    change
   end
 end
+# Changer.make_change(25)
